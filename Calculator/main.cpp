8d370ec607b1a8ed05dcef322220973b8df64c4c
Vlad Ishchuk <vlad.ischuck2006@gmail.com>
2023-09-14 18:58:49 +0300

    Update main.cpp
    Added the ability to extend the session

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -10,43 +10,62 @@
 using namespace std;
 
 int main() {
-    // Оголошуємо зміні
-    double first_number, second_number;
-    char operation;
-    double result = 0;
+    while (true) {
+        // Оголошуємо зміні
+        double first_number, second_number;
+        char operation;
+        double result = 0;
 
-    // Запитуємо дані користувача
-    cout << "Enter first number: ";
-    cin >> first_number;
-    cout << "Enter second number: ";
-    cin >> second_number;
-    cout << "Choose operation: ";
-    cin >> operation;
-    
-    switch (operation) {
-        case 43:
-            // Додавання: виконуємо операцію додавання між першим і другим числами
-            result = first_number + second_number;
-            break;
-        case 45:
-            // Віднімання: виконуємо операцію віднімання першого числа від другого
-            result = first_number - second_number;
-            break;
-        case 42:
-            // Множення: виконуємо операцію множення першого і другого чисел
-            result = first_number * second_number;
-            break;
-        case 47:
-            // Ділення: виконуємо операцію ділення першого числа на друге
-            result = first_number / second_number;
-            break;
-        default:
-            // Невідома операція: виводимо повідомлення про помилку
-            std::cout << "The specified operator is not valid" << std::endl;
-            break;
+        // Запитуємо дані користувача
+        cout << "Enter first number: ";
+        cin >> first_number;
+        cout << "Enter second number: ";
+        cin >> second_number;
+        cout << "Choose operation: ";
+        cin >> operation;
+        
+        switch (operation) {
+            case 43:
+                // Додавання: виконуємо операцію додавання між першим і другим числами
+                result = first_number + second_number;
+                break;
+            case 45:
+                // Віднімання: виконуємо операцію віднімання першого числа від другого
+                result = first_number - second_number;
+                break;
+            case 42:
+                // Множення: виконуємо операцію множення першого і другого чисел
+                result = first_number * second_number;
+                break;
+            case 47:
+                // Ділення: виконуємо операцію ділення першого числа на друге
+                result = first_number / second_number;
+                break;
+            default:
+                // Невідома операція: виводимо повідомлення про помилку
+                std::cout << "The specified operator is not valid." << std::endl;
+                return 0;
+                break;
+        }
+
+        // Виводимо результат
+        cout << "The result is: " << result << std::endl;
+        
+        // Питаємо користувача, чи він бажає продовжити
+        string continue_answer;
+        std::cout << "Want to continue? (y/n): ";
+        cin >> continue_answer;
+        
+        // Перевіряємо відповідь користувача
+        if (continue_answer == "y" or continue_answer == "yes") {
+            std::cout << std::endl;
+        } else if (continue_answer == "n" or continue_answer == "no") {
+            return 0;
+        } else {
+            std::cout << "The answer is not valid." << std::endl;
+            return 0;
+        }
     }
-
-    // Виводимо результат
-    cout << "The result is: " << result << std::endl;
+    
     return 0;
 }
